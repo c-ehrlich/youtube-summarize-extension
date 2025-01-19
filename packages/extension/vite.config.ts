a2b6b36ec6c8a5e384e379be7533c8ec010a81bb
c-ehrlich <ehrlich.christopher@gmail.com>
2025-01-19 23:17:31 +0100

    we have a working extension lol

--- packages/extension/vite.config.ts
+++ packages/extension/vite.config.ts
@@ -1,16 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
-import manifest from "./manifest.config.js";
-import { resolve } from "path";
+import manifest from "./manifest.json";
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), crx({ manifest })],
-  build: {
-    rollupOptions: {
-      input: {
-        popup: resolve(__dirname, "src/popup.html"),
-      },
-    },
-  },
 });

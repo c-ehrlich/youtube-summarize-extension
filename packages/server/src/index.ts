290a89dc4e7661a716c4f729a848aa3c09c747bb
c-ehrlich <ehrlich.christopher@gmail.com>
2025-06-28 11:34:16 +0700

    update prompt

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -47,12 +47,21 @@ api.post("/summary", ctxMiddleware, async (c) => {
 
   const summary = await generateText({
     model: c.var.openai("gpt-4o-mini-2024-07-18"),
-    prompt: generatePrompt({
-      transcript: transcript,
-      title: title,
-      channel: author,
-      description: description,
-    }),
+    messages: [
+      {
+        role: "system",
+        content: systemPrompt,
+      },
+      {
+        role: "user",
+        content: generateUserPrompt({
+          transcript: transcript,
+          title: title,
+          channel: author,
+          description: description,
+        }),
+      },
+    ],
   });
 
   c.var.axiom.ingest({
@@ -79,7 +88,27 @@ app.route("/api", api);
 
 export default app;
 
-function generatePrompt({
+const systemPrompt = `
+<identity>
+You are a helpful assistant that summarizes and extracts insights from YouTube videos.
+</identity>
+
+<instructions>
+Summarize the top takeaways from the video. The goal is that the user can read the summary and get all the main information.
+
+Some examples on how to be useful:
+- If the title is a question or some other kind of clickbait that is answered in the video, start by answering it with 1 sentence.
+- Keep it short and concise - in most cases you should not need more than 5 bullet points.
+- But you can make exceptions - for example if the video is a top10, tell me what the top10 things are and why. Return the summary in a markdown format (but no need to use a code block).
+- Give specific insights.
+  - Bad: "The host takes both shoes for a run, sharing insights on their performance and suitability for speed training."
+  - Good: "The host takes both shoes for a run, and concludes that the Adidas shoe is faster but the Saucony shoe is a better fun fast shoe for most runners."
+- If the video is a review, tell me the score, and what the pros and cons are, as well as comparisons to other products if the reviewer made them.
+</instructions>
+
+Please use the following information to help you give a good summary:`;
+
+function generateUserPrompt({
   transcript,
   title,
   channel,
@@ -97,32 +126,28 @@ function generatePrompt({
     transcript,
   });
 
-  return `You are a helpful assistant that summarizes YouTube videos.
-If the title is a question or some other kind of clickbait, start by answering it with 1 sentence.
-Keep it short and concise - in most cases you should not need more than 5 bullet points.
-But you can make exceptions - for example if the video is a top10, tell me what the top10 things are and why. Return the summary in a markdown format (but no need to use a code block).
-
-Please use the following information to help you give a good summary:
-
+  return `
+<context>
 ${
   channel &&
-  `Channel name:
+  `## Channel name:
 ${channel}`
 }
 
 ${
   title &&
-  `Video title:
+  `## Video title:
 ${title}`
 }
 
 ${
   description &&
-  `Video description:
+  `## Video description:
 ${description}`
 }
 
-Transcript:
+## Transcript:
 ${transcript}
+</context>
 `;
 }
